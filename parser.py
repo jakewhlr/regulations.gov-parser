00cc46734cc889eb793edd0e94cc98a1416ac448
Jake Wheeler <jakewhlr@gmail.com>
2019-04-08 14:20:16 -0700

    added command line arguments + verbosity

--- parser.py
+++ parser.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python3
+import requests, csv, time, sys, json, re, argparse
+import logging as log
+parser = argparse.ArgumentParser()
+parser.add_argument("n_comments", type = int, help = "Number of comments to grab")
+parser.add_argument('-v', '--verbose', action = 'count', default = 0)
+args = parser.parse_args()
 
-import requests, csv, time, sys, json, re
+if args.verbose:
+    log.basicConfig(format = "%(levelname)s: %(message)s", level = log.DEBUG)
+    log.info("Verbose output.")
+else:
+    log.basicConfig(format = "%(levelname)s: %(message)s")
+
 
 api_key = '23jOs9uefHRVxsHzKbAP7kGBAi9S5LGlOYAJMrZx'
 docket_id = 'ED-2018-OCR-0064'
-total_docs = 2000
+total_docs = args.n_comments
 docs_per_page = 1000
 requests_list = []
 parsed_list = []
 states = []
-# regex_string = '(\d+)(.+)\n((.)+),(.+) (\d{5})(-(\d{4}))?'
-# regex_string = '(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New\sHampshire|New\sJersey|New\sMexico|New\sYork|North\sCarolina|North\sDakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode\sIsland|South\sCarolina|South\sDakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West\sVirginia|Wisconsin|Wyoming)|(A[LKZR])|((C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))'
+
 regex_string = '^(.+),(.+) (\d{5})$'
 
 for i in range(int(total_docs / docs_per_page)):
-    print("Getting page", i)
+    log.info("Getting page " + str(i))
     parsed_list.append(json.loads(requests.get("https://api.data.gov:443/regulations/v3/documents.json",
                      params={
                              "api_key": api_key,
@@ -30,7 +40,7 @@ with open('output.csv', 'w') as csvfile:
     writer.writeheader()
     for i, page in enumerate(parsed_list):
         for j, comment in enumerate(page['documents']):
-            print("Parsing page", i, " comment", j)
+            log.info("Parsing page " +  str(i) + " comment " + str(j))
             dict_line = {'author': None, 'datetime': None, 'comment': None}
             if 'submitterName' in comment.keys():
                 dict_line['author'] = comment['submitterName']
