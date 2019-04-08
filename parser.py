d14ae599808006dd3cc23a8fa752f6166c185941
Jake Wheeler <jakewhlr@gmail.com>
2019-04-08 14:10:52 -0700

    added city/state/zip regex

--- parser.py
+++ parser.py
@@ -4,13 +4,14 @@ import requests, csv, time, sys, json, re
 
 api_key = '23jOs9uefHRVxsHzKbAP7kGBAi9S5LGlOYAJMrZx'
 docket_id = 'ED-2018-OCR-0064'
-total_docs = 53000
+total_docs = 2000
 docs_per_page = 1000
 requests_list = []
 parsed_list = []
 states = []
 # regex_string = '(\d+)(.+)\n((.)+),(.+) (\d{5})(-(\d{4}))?'
-regex_string = '(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New\sHampshire|New\sJersey|New\sMexico|New\sYork|North\sCarolina|North\sDakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode\sIsland|South\sCarolina|South\sDakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West\sVirginia|Wisconsin|Wyoming)|(A[LKZR])|((C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))'
+# regex_string = '(Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New\sHampshire|New\sJersey|New\sMexico|New\sYork|North\sCarolina|North\sDakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode\sIsland|South\sCarolina|South\sDakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West\sVirginia|Wisconsin|Wyoming)|(A[LKZR])|((C[AOT])|(D[EC])|(FL)|(GA)|(HI)|(I[DLNA])|(K[SY])|(LA)|(M[EDAINSOT])|(N[EVHJMYCD])|(O[HKR])|(PA)|(RI)|(S[CD])|(T[NX])|(UT)|(V[TA])|(W[AVIY]))'
+regex_string = '^(.+),(.+) (\d{5})$'
 
 for i in range(int(total_docs / docs_per_page)):
     print("Getting page", i)
@@ -33,19 +34,13 @@ with open('output.csv', 'w') as csvfile:
             dict_line = {'author': None, 'datetime': None, 'comment': None}
             if 'submitterName' in comment.keys():
                 dict_line['author'] = comment['submitterName']
-                # print(comment['submitterName'])
 
             if 'postedDate' in comment.keys():
                 dict_line['datetime'] = comment['postedDate']
 
             if 'commentText' in comment.keys():
                 dict_line['comment'] = comment['commentText']
-                match = re.search(regex_string, comment['commentText'])
+                match = re.search(regex_string, comment['commentText'], re.MULTILINE)
                 if match:
-                    matches = match.groups()
-                    filtered = filter(None, matches)
-                    states = []
-                    for state in filtered:
-                        states.append(state)
-                    dict_line['state'] = states[0]
+                    dict_line['state'] = match.group()
             writer.writerow(dict_line)
